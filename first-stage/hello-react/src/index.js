ebd5520d636f263c91b8f059c9afcec424ee7aca
木水 <lc564128352@gmail.com>
2018-09-28 21:06:57 +0800

    6.创建新的 react demo
    1.使用的 npm 版本号为6.4.1, node 版本号为 8.12.0
    2.重新创建 react demo
    3.react 小书第13小节，渲染列表数据完成

---
+++ first-stage/hello-react/src/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+const users = [
+    { username: 'Jerry', age: 21, gender: 'male' },
+    { username: 'Tomy', age: 22, gender: 'male' },
+    { username: 'Lily', age: 19, gender: 'female' },
+    { username: 'Lucy', age: 20, gender: 'female' }
+]
+
+class User extends Component {
+    render () {
+        const { user } = this.props
+        return (
+            <div>
+                <div>姓名：{user.username}</div>
+                <div>年龄：{user.age}</div>
+                <div>性别：{user.gender}</div>
+                <hr />
+            </div>
+        )
+    }
+}
+
+class Index extends Component {
+    render () {
+        return (
+            <div>
+                {users.map((user, i) => <User key={i} user={user} />)}
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Index />,
+    document.getElementById('root')
+)
