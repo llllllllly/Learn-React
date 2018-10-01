41c65c3ae1f28ba54a93ceae4f0c6d725b98dc7a
木水 <lc564128352@gmail.com>
2018-10-01 14:25:24 +0800

    11.React 小书第二阶段实战完成
    1.页面加载完成自动聚焦到评论输入框。
    2.把用户名持久化。
    3.把已经发布的评论持久化。
    4.评论显示发布日期。
    5.类似 Markdown 的行内代码块显示功能。

--- first-stage/comment-app/src/CommentInput.js
+++ first-stage/comment-app/src/CommentInput.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class CommentInput extends Component {
+    static propTypes = {
+        onSubmit: PropTypes.func
+    }
+
     constructor() {
         super()
         this.state = {
             username: '',
             content: ''
         }
+    }
+
+    componentDidMount() {
+        this.textarea.focus()
+    }
+
+    componentWillMount() {
+        this._loadUsername();
+    }
+
+    _loadUsername() {
+        const username = localStorage.getItem('username')
+        if (username) {
+            this.setState({ username })
+        }
+    }
+
+    _saveUsername(username) {
+        localStorage.setItem('username', username)
+    }
+
+    handleUsernameBlur(event) {
+        this._saveUsername(event.target.value)
     }
 
     handleUsernameChange(event) {
@@ -23,8 +51,11 @@ class CommentInput extends Component {
 
     handleSubmit() {
         if (this.props.onSubmit) {
-            const { username, content } = this.state
-            this.props.onSubmit({username, content})
+            this.props.onSubmit({
+                username: this.state.username,
+                content: this.state.content,
+                date: +new Date()
+            })
         }
         this.setState({
             content: ''
@@ -37,13 +68,13 @@ class CommentInput extends Component {
                 <div className='comment-field'>
                     <span className='comment-field-name'>用户名：</span>
                     <div className='comment-field-input'>
-                        <input value={this.state.username} onChange={this.handleUsernameChange.bind(this)} />
+                        <input value={this.state.username} onBlur={this.handleUsernameBlur.bind(this)} onChange={this.handleUsernameChange.bind(this)} />
                     </div>
                 </div>
                 <div className='comment-field'>
                     <span className='comment-field-name'>评论内容：</span>
                     <div className='comment-field-input'>
-                        <textarea value={this.state.content} onChange={this.handleContentChange.bind(this)} />
+                        <textarea ref={(textarea) => this.textarea = textarea} value={this.state.content} onChange={this.handleContentChange.bind(this)} />
                     </div>
                 </div>
                 <div className='comment-field-button'>
