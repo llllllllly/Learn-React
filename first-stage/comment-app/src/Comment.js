057eebd96949707ae4ed4c8dec65ce3144026238
木水 <lc564128352@gmail.com>
2018-10-01 12:16:12 +0800

    10.添加 PropTypes 模块
    1.添加 PropTypes 模块
    2.使用 PropTypes 对组件参数进行验证

--- first-stage/comment-app/src/Comment.js
+++ first-stage/comment-app/src/Comment.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class Comment extends Component {
+    static propTypes = {
+        comment: PropTypes.object.isRequired
+    }
     render() {
         const { comment } = this.props 
         return (
