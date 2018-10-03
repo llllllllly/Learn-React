b14f7850e925d1a03e2c6ad5077c9b3b6e87ae34
木水 <lc564128352@gmail.com>
2018-10-03 13:52:28 +0800

    15.结合 context 和 store
    1.动手实现 React-redux（二）：结合 context 和 store

--- first-stage/make-react-redux/src/index.js
+++ first-stage/make-react-redux/src/index.js
@@ -4,17 +4,48 @@ import ReactDOM from 'react-dom';
 import Header from './Header'
 import Content from './Content'
 import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+function createStore(reducer) {
+    let state = null
+    const listeners = []
+    const subscribe = (listener) => listeners.push(listener)
+    const getState = () => state
+    const dispatch = (action) => {
+        state = reducer(state, action)
+        listeners.forEach((listener) => listener())
+    }
+    dispatch({})
+    return { getState, dispatch, subscribe }
+}
 
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: http://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+const themeReducer = function (state, action) {
+    if (!state) {
+        return {
+            themeColor: 'red'
+        }
+    }
+    switch (action.type) {
+        case 'CHANGE_COLOR':
+            return {
+                ...state,
+                themeColor: action.themeColor
+            }
+        default:
+            return state
+    }
+}
+
+const store = createStore(themeReducer)
 
 class Index extends Component {
+    static childContextTypes = {
+        store: PropTypes.object
+    }
+
+    getChildContext() {
+        return { store }
+    }
+
     render() {
         return (
             <div>
